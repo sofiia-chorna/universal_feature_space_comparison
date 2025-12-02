01b2d9512ee1c7346c05f58b57cad2c46b37a62e
Sofiia Chorna <sofiia.chorna@epfl.ch>
2025-12-02 15:22:30 +0100

    Head fine tuning files

---
+++ scripts/fine-tuning/hf/job-c.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=da_Li
+#SBATCH --nodes=1
+#SBATCH --time=20:30:00
+#SBATCH --gres=gpu:1
+#SBATCH --partition=h100 
+
+source ~/miniforge3/bin/activate 
+conda activate cartospeed_2025_9
+
+mtt train  options-c.yaml  
+#mtt train --restart auto options-c.yaml  
