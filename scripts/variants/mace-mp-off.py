c48f1efb4cba9a7b96750a133b315753cda7e6f8
Sofiia Chorna <sofiia.chorna@epfl.ch>
2025-12-02 13:05:34 +0100

    Mace variants

---
+++ scripts/variants/mace-mp-off.py
@@ -0,0 +1,56 @@
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
+import json
+
+import numpy as np
+
+from src.errors import compute_model_vs_model
+from src.utils import get_stratified_indices
+
+KEY = "mace-mp-off"
+SAVE_DIR = "./results/reconstruction_errors/variants/mace/mace_mp_off"
+os.makedirs(SAVE_DIR, exist_ok=True)
+
+
+FEAT_PATHS = {
+    "mp-0b3": "data/features/mad/umlips/mace/variants/mace-mp-0b3-medium_organic.npy",
+    "off23": "data/features/mad/umlips/mace/off23/mace-off23-medium.npy",
+}
+
+
+def main(error_type="LFRE"):
+    model_features = {name.upper(): np.load(path) for name, path in FEAT_PATHS.items()}
+
+    for name, feats in model_features.items():
+        print(name, feats.shape)
+
+    global_test_indices = get_stratified_indices(
+        "data/xyz/mad-test-consistent-organic.xyz"
+    )
+
+    out_file = os.path.join(SAVE_DIR, f"{error_type.lower()}_{KEY}.json")
+
+    results = compute_model_vs_model(
+        model_features,
+        error_type=error_type,
+        save_path=out_file,
+        test_indices=global_test_indices,
+    )
+
+    with open(out_file, "w") as f:
+        json.dump(results, f, indent=2)
+    print(f"[Saved results to {out_file}]")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python run.py <LFRE|GFRE>")
+        sys.exit(1)
+
+    error_type = sys.argv[1].upper()
+    main(error_type)
---
+++ scripts/variants/mace-sizes-mp.py
@@ -0,0 +1,55 @@
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
+import json
+
+import numpy as np
+
+from src.errors import compute_model_vs_model
+from src.utils import get_stratified_indices
+
+KEY = "mace-mp-a0"
+SAVE_DIR = "./results/reconstruction_errors/variants/mace/sizes"
+os.makedirs(SAVE_DIR, exist_ok=True)
+
+
+FEAT_PATHS = {
+    "small": "data/features/mad/umlips/mace/mp-a0/mace-mp-0a-small.npy",
+    "medium": "data/features/mad/umlips/mace/mp-a0/mace-mp-0a-medium.npy",
+    "large": "data/features/mad/umlips/mace/mp-a0/mace-mp-0a-large.npy",
+}
+
+
+def main(error_type="LFRE"):
+    model_features = {name.upper(): np.load(path) for name, path in FEAT_PATHS.items()}
+
+    for name, feats in model_features.items():
+        print(name, feats.shape)
+
+    global_test_indices = get_stratified_indices("data/xyz/mad-test-consistent.xyz")
+
+    out_file = os.path.join(SAVE_DIR, f"{error_type.lower()}_{KEY}.json")
+
+    results = compute_model_vs_model(
+        model_features,
+        error_type=error_type,
+        save_path=out_file,
+        test_indices=global_test_indices,
+    )
+
+    with open(out_file, "w") as f:
+        json.dump(results, f, indent=2)
+    print(f"[Saved results to {out_file}]")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python run.py <LFRE|GFRE>")
+        sys.exit(1)
+
+    error_type = sys.argv[1].upper()
+    main(error_type)
